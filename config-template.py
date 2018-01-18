158bf101d03e83a3b7bf42a32bb323ed82e0a0b6
Christopher Berdahl <christopher.berdahl@binops.co.uk>
2018-01-18 22:31:35 +0000

    Feat: Add new config.py file with all secret keys

---
+++ config-template.py
@@ -0,0 +1,34 @@
+# project/server/config.py
+
+'''
+This template contains all the configurations for different environments
+The following classes are used according to their environment:
+	* All: BaseConfig
+	* Development: DevelopmentConfig
+	* Testing: TestingConfig
+	* Production: ProductionConfig
+'''
+
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class BaseConfig(object):
+    ''' Base configuration '''
+    SECRET_KEY = 'very_secret_key'
+    DEBUG = False
+    TESTING = False
+    # Google OAuh2.0
+    CLIENT_ID = '101XXXXXXX-l3hXXXXXXXX8e.apps.googleusercontent.com '
+    CLIENT_SECRET = 'tVXXXXXXXXJ7 '
+
+
+class DevelopmentConfig(BaseConfig):
+    ''' Development configuration '''
+    DEBUG = True
+
+
+class TestingConfig(BaseConfig):
+    ''' Testing configuration '''
+    DEBUG = False
+    TESTING = True
--- project/server/__init__.py
+++ project/server/__init__.py
@@ -1,17 +1,31 @@
 # project/server/__init__.py
 
-''' Flask is imported and basic configurations are set. '''
+import os
+import pdb
+
 from flask import Flask
+
+
+''' Create Flask instance and set Application Environment
+through the APP_SETTINGS environmental variable.
+'''
 
 app = Flask(__name__,
             template_folder='../client/templates',
-            static_folder='../client/static'
+            static_folder='../client/static',
+            instance_relative_config=True
             )
+
+app_settings = os.getenv(
+    'FLASK_SETTINGS', 'config.DevelopmentConfig')
+app.config.from_object(app_settings)
+app.config.from_envvar('FLASK_CONFIG')
 
 
 ''' Blueprints
 All the different packages are imported and
 the blueprints are registered in the app.
 '''
+
 from project.server.main.views import main_blueprint
 app.register_blueprint(main_blueprint)
