6b36d674d6922bdff3470df5f2729e10ccfcec0d
Christopher Berdahl <christopher.berdahl@binops.co.uk>
2018-01-25 01:08:41 +0000

    Doc: Change page size and font size

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -122,11 +122,11 @@ htmlhelp_basename = 'Catalogdoc'
 latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
     #
-    # 'papersize': 'letterpaper',
+    'papersize': 'a4paper',
 
     # The font size ('10pt', '11pt' or '12pt').
     #
-    # 'pointsize': '10pt',
+    'pointsize': '10pt',
 
     # Additional stuff for the LaTeX preamble.
     #
