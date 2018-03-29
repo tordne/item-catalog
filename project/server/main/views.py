2fb334ac58ae70c4836818c5ec7b4b2e1ccc508a
Christopher Berdahl <christopher.berdahl@binops.co.uk>
2018-03-29 01:31:25 +0100

    Doc Add sphinx styled documentation to classes and routes

--- project/server/main/views.py
+++ project/server/main/views.py
@@ -12,22 +12,44 @@ main_blueprint = Blueprint('main', __name__,)
 ''' Public Routes '''
 
 
-# Route listing all the categories
 @main_blueprint.route('/')
 @main_blueprint.route('/catalog')
 def list_categories():
+    '''
+    List all the categories and last added items
+
+    .. :quickref: Home; List all the categories
+
+    :return: Render the catalog template
+    '''
     return render_template('main/catalog.html')
 
 
-# Route showing all the items under category
 @main_blueprint.route('/catalog/<string:category>')
 def list_items(category):
+    '''
+    List all the items in the given category
+
+    .. :quickref: Category; List all the items
+
+    :param str category: The selected category
+    :return: Render the items template
+    '''
     return render_template('main/items.html', category=category)
 
 
 # Route showing information about the item
 @main_blueprint.route('/catalog/<string:category>/<string:item>')
 def info_item(category, item):
+    '''
+    List the information about the given item
+
+    .. :quickref: Item; List the item info
+
+    :param str category: The selected category
+    :param str item: The selected item
+    :return: Render the description template
+    '''
     return render_template(
         'main/description.html',
         category=category,
@@ -37,35 +59,70 @@ def info_item(category, item):
 ''' Private Routes '''
 
 
-# Create a new Category
 @main_blueprint.route('/catalog/new')
 def category_new():
+    '''
+    Create a new category
+
+    .. :quickref: Category; Create a new category
+
+    :return: Render the category_new template
+    '''
     return render_template('main/category_new.html')
 
 
-# Route to edit the category
 @main_blueprint.route('/catalog/<string:category>/edit')
 def category_edit(category):
+    '''
+    Edit the given category
+
+    .. :quickref: Category; Edit the given category
+
+    :param str category: The selected category
+    :return: Render the category_edit template
+    '''
     return render_template('main/category_edit.html', category=category)
 
 
-# Route to delete the category
 @main_blueprint.route('/catalog/<string:category>/delete')
 def category_delete(category):
+    '''
+    Delete the given category
+
+    .. :quickref: Category; Delete the given category
+
+    :param str category: The selected category
+    :return: Render the category_delete template
+    '''
     return render_template('main/category_delete.html', category=category)
 
 
-# Create a new item
 @main_blueprint.route('/catalog/<string:category>/new')
 def item_new(category):
+    '''
+    Create a new item
+
+    .. :quickref: Item; Create a new item
+
+    :param str category: The selected category
+    :return: Render the item_new template
+    '''
     return render_template(
         'main/item_new.html',
         category=category)
 
 
-# Route to edit the item
 @main_blueprint.route('/catalog/<string:category>/<string:item>/edit')
 def item_edit(category, item):
+    '''
+    Edit the given item
+
+    .. :quickref: Item; Edit the given item
+
+    :param str category: The selected category
+    :param str item: The selected item
+    :return: Render the item_edit template
+    '''
     return render_template(
         'main/item_edit.html',
         category=category,
@@ -75,6 +132,15 @@ def item_edit(category, item):
 # Route to delete the item
 @main_blueprint.route('/catalog/<string:category>/<string:item>/delete')
 def item_delete(category, item):
+    '''
+    Delete the given item
+
+    .. :quickref: Item; Delete the given item
+
+    :param str category: The selected category
+    :param str item: The selected item
+    :return: Render the item_delete template
+    '''
     return render_template(
         'main/item_delete.html',
         category=category,
--- project/server/models.py
+++ project/server/models.py
@@ -1,4 +1,8 @@
 # project/server/models.py
+'''
+.. module:: models
+    :synopsis; All the important classes
+'''
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
@@ -8,6 +12,16 @@ Base = declarative_base()
 
 
 class User(Base):
+    '''
+    The User class contains 3 attributes id, name, email
+
+    .. py:attribute:: id
+        Integer, primary_key=True
+    .. py:attribute:: name
+        String(250), nullable=False
+    .. py:attribute:: email
+        String(250), nullable=False
+    '''
     __tablename__ = 'user'
 
     id = Column(Integer, primary_key=True)
@@ -16,6 +30,19 @@ class User(Base):
 
 
 class Category(Base):
+    '''
+    The Category class has has 2 basic attributes id, name
+    The 3rd attribute is the relationship with 'User' table
+
+    .. py:attribute:: id
+        Integer, primary_key=True
+    .. py:attribute:: name
+        String(250), nullable=False
+    .. py:attribute:: user_id
+        Integer, ForeignKey('user.id')
+    .. py:attribute:: user
+        relationship(User
+    '''
     __tablename__ = 'category'
 
     id = Column(Integer, primary_key=True)
@@ -25,6 +52,27 @@ class Category(Base):
 
 
 class Item(Base):
+    '''
+    The Item class has 3 basic attr. id, name, description
+    The 3rd and 4th attr. are relationships to the User and Category tables
+
+    .. py:attribute:: id
+        Integer, primary_key=True
+    .. py:attribute:: name
+        String(250), nullable=False
+    .. py:attribute:: description
+        String(250)
+
+    .. py:attribute:: category_id
+        Integer, ForeignKey('category.id')
+    .. py:attribute:: category
+        relationship(Category)
+
+    .. py:attribute:: user_id
+        Integer, ForeignKey('user.id')
+    .. py:attribute:: user
+        relationship(User
+    '''
     __tablename__ = 'item'
 
     id = Column(Integer, primary_key=True)
