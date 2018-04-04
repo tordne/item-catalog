b5f44a6a4d423296a910797f45d2c7fa36b424f6
Christopher Berdahl <christopher.berdahl@binops.co.uk>
2018-04-04 14:50:41 +0100

    Chore: Move the CLIENT_SECRET_FILE to an env variable

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ class BaseConfig(object):
     # Google OAuh2.0
     CLIENT_ID = '1018639020947-l3h0bppvstv799dijbdiuot9a86bce8e.apps.googleusercontent.com'  # NOQA
     CLIENT_SECRET = 'tVHNcNnAf5drQJBBlcrTRhJ7'
-    CLIENT_SECRET_FILE = '/vagrant/client_secret.json'
+    CLIENT_SECRET_FILE = os.environ['CLIENT_SECRET_FILE']
     SCOPES = ['https://www.googleapis.com/auth/userinfo.profile',
           'https://www.googleapis.com/auth/userinfo.email']
     OAUTHLIB_INSECURE_TRANSPORT = '1'
@@ -30,7 +30,7 @@ class BaseConfig(object):
 class DevelopmentConfig(BaseConfig):
     ''' Development configuration '''
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI =os.environ['DATABASE_URL'] # NOQA
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL'] # NOQA
     OAUTHLIB_INSECURE_TRANSPORT = '1'
 
 
