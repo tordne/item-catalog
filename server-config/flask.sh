75f4375801131f1be1ce3f174dc45cbe97bccdc1
Christopher Berdahl <christopher.berdahl@binops.co.uk>
2018-04-04 21:15:24 +0100

    Chore: Add env variables for postgres and client_secret.json

--- server-config/flask.sh
+++ server-config/flask.sh
@@ -3,3 +3,4 @@ export FLASK_DEBUG=1
 export FLASK_CONFIG=/vagrant/config.py
 export FLASK_SETTINGS=config.DevelopmentConfig
 export CLIENT_SECRET_FILE=/vagrant/client_secret.json
+export DATABASE_URL=postgresql://vagrant:vagrant@localhost:5432/catalog
